8055a9c0b6f1d82000baf6195ce34e5287354646
Kupal <kravchenya@gmail.com>
2010-01-11 14:00:16 +0000

    skypeCommand: parameter converter

--- skypeCommand/src/com/kupal/skypeCommand/annotations/Converter.java
+++ skypeCommand/src/com/kupal/skypeCommand/annotations/Converter.java
@@ -2,9 +2,16 @@ package com.kupal.skypeCommand.annotations;
 
 import com.kupal.skypeCommand.annotations.process.ParameterConverter;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Defined object converter
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface Converter {
 
     Class<? extends ParameterConverter> value();
